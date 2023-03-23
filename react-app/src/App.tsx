33f6b607eb7d2c2482d2bdc65a1131ba73dbc6ff
Rakesh S R <rakeshsrking@gmail.com>
2023-03-23 14:36:11 +0530

    installing react app removing unwanted files

---
+++ react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const name: string = 'Rakesh';
+  return (
+    <div className="App">
+      {name}
+    </div>
+  );
+}
+
+export default App;
